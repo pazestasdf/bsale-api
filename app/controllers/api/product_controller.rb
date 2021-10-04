3b2ba58335f4e10bc2b71378e73c640d49f3072b
María Paz C <mpcarrascoojeda@gmail.com>
2021-10-04 11:49:33 -0300

    fix: @product controller add alias for product.name, shared name with category.name

--- app/controllers/api/product_controller.rb
+++ app/controllers/api/product_controller.rb
@@ -1,7 +1,7 @@
 module Api
     class ProductController < ApplicationController
         def index
-            products = Product.joins(:category).select('product.id, product.name, product.url_image, product.price, product.discount, category.name').order(category: :asc)
+            products = Product.joins(:category).select('product.id, product.name as nombre_producto, product.url_image, product.price, product.discount, category.name').order(category: :asc)
             render json: {status: 'success', message: 'Listado de Productos', data: products}, status: :ok
         end
     end
